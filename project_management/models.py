db5ee8ca4b0230714e951cf23ab144b22dff3185
CaptainKai <759317891@qq.com>
2018-06-13 18:04:37 +0800

    update PM model

--- project_management/models.py
+++ project_management/models.py
@@ -1,43 +1,51 @@
 from django.db import models
 
 # Create your models here.
-from utils.constants import FileType, ProType, ProState, ProLevel, ProStage
-import django.utils.timezone as timezone
+from utils.constants import ProState, ProLevel
 from time import strftime
 from account.models import User
 
 
 class SRTPProject(models.Model):
-    create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
+    create_year = models.DateField()
+    end_year = models.DateField()
+
     pro_state = models.CharField(max_length=10, default=ProState.RUNNING, choices=ProState.model_choices())
     pro_level = models.CharField(max_length=10, default=ProLevel.COLLEGE, choices=ProLevel.model_choices())
-    students = models.ManyToManyField(User, related_name='students_srtp')
+
+    person_in_charge = models.ForeignKey(User, related_name='PIC_srtp', on_delete=models.CASCADE)
+    members = models.CharField(max_length=500)
 
     file1 = models.FileField()
-    file2 = models.FileField()
-    file3 = models.FileField()
+    file2 = models.FileField(null=True)
+    file3 = models.FileField(null=True)
 
 
 class EduProject(models.Model):
-    create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
+    create_year = models.DateField()
+    end_year = models.DateField()
+
     pro_state = models.CharField(max_length=10, default=ProState.RUNNING, choices=ProState.model_choices())
     pro_level = models.CharField(max_length=10, default=ProLevel.COLLEGE, choices=ProLevel.model_choices())
 
-    members = models.ManyToManyField(User)
+    person_in_charge = models.ForeignKey(User, related_name='PIC_edu', on_delete=models.CASCADE)
+    members = models.CharField(max_length=500)
 
     file1 = models.FileField()
-    file2 = models.FileField()
-    file3 = models.FileField()
+    file2 = models.FileField(null=True)
+    file3 = models.FileField(null=True)
 
 
 class GraProject(models.Model):
-    create_time = models.DateTimeField(default=timezone.now)
-    update_time = models.DateTimeField(auto_now=True, null=True)
-    teacher = models.ForeignKey(User, related_name='teacher_gra', on_delete=models.CASCADE, null=True)
-    student = models.OneToOneField(User, related_name='student_gra', on_delete=models.CASCADE, null=True)
+    update_time = models.DateTimeField(auto_now=True)
+    create_year = models.DateField()
+    end_year = models.DateField()
+
+    teacher = models.ForeignKey(User, related_name='teacher_gra', on_delete=models.CASCADE)
+    student = models.OneToOneField(User, related_name='student_gra', on_delete=models.CASCADE)
 
     file1 = models.FileField()
-    file2 = models.FileField()
-    file3 = models.FileField()
+    file2 = models.FileField(null=True)
+    file3 = models.FileField(null=True)
